72b61e9d57d5c2356fe1cd4b033a0801c268611a
Jacobo <jacobo.londonod@uqvirtual.edu.co>
2026-02-15 23:10:17 -0500

    añadidas las funciones principales en la clase Workshop y agregada una clase Main unicamente para hacer pruebas

---
+++ src/main/java/co/edu/uniquindio/bikeworkshop/model/Main.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.bikeworkshop.model;
+
+import co.edu.uniquindio.bikeworkshop.model.Enums.MechanicSkillset;
+
+public class Main {
+    static void main() {
+        Workshop workshop = new Workshop("idc","311.253.356-5");
+
+    }
+
+
+
+}
